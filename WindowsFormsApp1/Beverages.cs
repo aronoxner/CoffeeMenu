62211558bbdb74b6e8bc17fdcba8372a4058539d
Aron Ochsner <aron.ochsner1@outlook.com>
2024-05-27 17:33:23 -0500

    Add project files.

---
+++ WindowsFormsApp1/Beverages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    internal abstract class Beverages
+    {
+        private string bevaragesize;
+        public string BeverageSize { get { return bevaragesize.ToUpper(); } set { bevaragesize = value; } }
+        public bool ToGo { get; set; }
+
+        public void OrderingSteps()
+        {
+            MessageBox.Show("What type of beverage would you like?");
+            MessageBox.Show("What size of beverage do you want?");
+            MessageBoxButtons
+        }
+    } 
+}
